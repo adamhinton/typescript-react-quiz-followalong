a5bca83e8fd453b5840e1f04b3d6b144967e9515
adamhinton <adam.d.hinton@gmail.com>
2022-06-06 23:00:53 -0400

    basic App html

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ const App = () => {
 
   const nextQuestion = () => {};
 
-  return <div className="App">Quiz</div>;
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+    </div>
+  );
 };
 
 export default App;

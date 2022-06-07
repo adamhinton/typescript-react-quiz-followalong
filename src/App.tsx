ad96d421e5a038e5202ce002d948c1dcc1361b21
adamhinton <adam.d.hinton@gmail.com>
2022-06-06 22:57:34 -0400

    scaffold startTrivia() and checkAnswer()

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
   return <div className="App">Quiz</div>;
 };
 

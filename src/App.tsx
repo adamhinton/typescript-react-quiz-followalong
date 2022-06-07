b0a09de1c3e3197e85cd7c5352b5f979948731aa
adamhinton <adam.d.hinton@gmail.com>
2022-06-06 23:01:57 -0400

    flesh out buttons

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 };

1e5312790e1d8068c329f18c2788b75e216896dc
adamhinton <adam.d.hinton@gmail.com>
2022-06-08 22:24:19 -0400

    endpoint variable in API.ts

--- src/API.ts
+++ src/API.ts
@@ -7,4 +7,6 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
-) => {};
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+};

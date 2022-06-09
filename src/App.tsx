c0382ff7927b986d91baa3f55273ba9a6b9cd9cb
adamhinton <adam.d.hinton@gmail.com>
2022-06-08 22:27:28 -0400

    temp comment unneeded code

--- src/App.tsx
+++ src/App.tsx
@@ -27,14 +27,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
